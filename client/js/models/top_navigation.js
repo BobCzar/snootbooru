d7ed45b759ae40eeb3ce01ded0701768d2d1f328
Orange <105612502+TheManOfOrange@users.noreply.github.com>
2024-05-30 21:33:56 -0400

    Update top_navigation.js

--- client/js/models/top_navigation.js
+++ client/js/models/top_navigation.js
@@ -80,8 +80,7 @@ function _makeTopNavigation() {
     const ret = new TopNavigation();
     ret.add("home", new TopNavigationItem("H", "Home", ""));
     ret.add("posts", new TopNavigationItem("P", "Posts", "posts"));
-    const url = 'https://mods.snootbooru.com/';
-    ret.add("Mods", new TopNavigationItem("M", "Mods", ""));
+    ret.add("Mods", new TopNavigationItem("M", "Mods", "mods"));
     ret.add("upload", new TopNavigationItem("U", "Upload", "upload"));
     ret.add("comments", new TopNavigationItem("C", "Comments", "comments"));
     ret.add("tags", new TopNavigationItem("T", "Tags", "tags"));
